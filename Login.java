7425927b6e0011c405303de04e96a645b8e06c4b
VijayVittal123 <112443684+VijayVittal123@users.noreply.github.com>
2022-11-09 14:55:55 -0500

    Update Login.java

--- Login.java
+++ Login.java
@@ -7,7 +7,8 @@ public class login {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> usernameArrayList = new ArrayList<>();
         ArrayList<String> passwordArrayList = new ArrayList<>();
-        HashMap<String, String> login1 = new HashMap<String, String>();
+        HashMap<String, String> sellerLogin = new HashMap<String, String>();
+        HashMap<String, String> buyerLogin = new HashMap<String, String>();
         System.out.println("Welcome");
         boolean flag=false;
         // if you create an account then go back to beginning and ask if they want to create a new account or login with existing one
@@ -15,7 +16,7 @@ public class login {
             flag=false;
             // ask if they have an exisiting or new account
             System.out.println("Do you have an existing account or would you like to create a new account?");
-        System.out.println("1. Existing account\n2.Create new account");
+        System.out.println("1.Existing account\n2.Create new account");
         String newOrExisting = scanner.nextLine();
         // if 2 create a new account and ask if they are seller or buyer
         if (newOrExisting.equals("2")) {
@@ -29,7 +30,7 @@ public class login {
                 System.out.println("Enter your new password");
                 String password = scanner.nextLine();
                 passwordArrayList.add(password);
-                login1.put(username, password);
+                sellerLogin.put(username, password);
                 System.out.println("Enter the stores (separate each store by a comma(,))");
                 String stores = scanner.nextLine();
                 if (stores.contains(",")) {
@@ -44,28 +45,47 @@ public class login {
                 System.out.println("Enter your new password");
                 String password = scanner.nextLine();
                 passwordArrayList.add(password);
-                login1.put(username, password);
+                buyerLogin.put(username, password);
                 System.out.println("Account made!");
             }
             flag=true;
             // if 1 is input then you try to login
-            
+
         } else if (newOrExisting.equals("1")) {
-            
-            System.out.println("Enter your username");
-            String username = scanner.nextLine();
-            // if username is not located in hasmap then show an error
-            if (login1.containsKey(username)) {
-                System.out.println("Enter your password");
-                String password = scanner.nextLine();
-                // if password and username are not in hashmap, say password is incorrect
-                if (login1.containsKey(username) && login1.containsValue(password)) {
-                    System.out.println("Login successful!");
-                } else {
-                    System.out.println("Password is incorrect");
+            System.out.println("Do you want to login as a Seller or Buyer");
+            String sellerOrBuyer=scanner.nextLine();
+            if (sellerOrBuyer.equalsIgnoreCase("Seller")) {
+                System.out.println("Enter your username");
+                String username = scanner.nextLine();
+                // if username is not located in hasmap then show an error
+                if (sellerLogin.containsKey(username)){
+                    System.out.println("Enter your password");
+                    String password = scanner.nextLine();
+                    // if password and username are not in hashmap, say password is incorrect
+                    if (sellerLogin.containsKey(username) && sellerLogin.containsValue(password)) {
+                        System.out.println("Login successful!");
+                    } else {
+                        System.out.println("Password is incorrect");
+                    }
+                } else{
+                    System.out.println("Username not found");
                 }
-            } else {
-                System.out.println("Username not found");
+            } else if (sellerOrBuyer.equalsIgnoreCase("Buyer")) {
+                System.out.println("Enter your username");
+                String username = scanner.nextLine();
+                // if username is not located in hasmap then show an error
+                if (sellerLogin.containsKey(username)){
+                    System.out.println("Enter your password");
+                    String password = scanner.nextLine();
+                    // if password and username are not in hashmap, say password is incorrect
+                    if (sellerLogin.containsKey(username) && sellerLogin.containsValue(password)) {
+                        System.out.println("Login successful!");
+                    } else {
+                        System.out.println("Password is incorrect");
+                    }
+                } else{
+                    System.out.println("Username not found");
+                }
             }
 
 
