c48743c810e58cf618cda652b042a2913453a4b7
rohanmenon1 <menon67@Purdue.edu>
2022-11-14 17:29:51 -0500

    modified marketplace

--- MarketPlaceTest.java
+++ MarketPlaceTest.java
@@ -44,7 +44,7 @@ public class MarketPlaceTest {
                 + "Enter your new password\n"
                 + "Enter your e-mail\n"
                 + "What is the filepath for the statistics of the seller\n"
-                + "Account made!\n"
+                + "Account made!\n" 
                 + "Would you like you login or create a new account?\n"
                 + "1.Login\n"
                 + "2.Create new account\n"
@@ -87,76 +87,6 @@ public class MarketPlaceTest {
         assertEquals("Ensure that your results match the format of the ones given in the handout!",
                 expected.trim(), output.trim());
     }
-    public void testTwo() {
-        String input = "2" + System.lineSeparator() + "buyer" + System.lineSeparator() + "Jake" +
-                System.lineSeparator() + "green" + System.lineSeparator() + "jake@gmail.com" + System.lineSeparator()
-                + "purchaseHistory.txt" + System.lineSeparator() + "shoppingCart.txt" + System.lineSeparator() + "2"  + System.lineSeparator()
-                + "Jake" + System.lineSeparator() + "Will" + System.lineSeparator() + "yellow" + System.lineSeparator()
-                + "jake@gmail.com" + System.lineSeparator() + "purchaseHistory2.txt" + System.lineSeparator() + "shoppingCart2.txt" + System.lineSeparator()
-                + "1" + System.lineSeparator() + "buyer" + System.lineSeparator() + "Jake" + System.lineSeparator()
-                + "yellow" + System.lineSeparator() + "600" + System.lineSeparator() + "500" + System.lineSeparator()
-                + "700" + System.lineSeparator() + "1000" + System.lineSeparator() + "350" + System.lineSeparator()
-                + "550" + System.lineSeparator() + "550" + System.lineSeparator() + "850" + System.lineSeparator();
 
-        // Expected result
-        String expected = "Welcome\n"
-                + "Would you like you login or create a new account?\n"
-                + "1.Login\n"
-                + "2.Create new account\n"
-                + "Are you a Seller or Buyer\n"
-                + "Enter your new username\n"
-                + "Enter your new password\n"
-                + "Enter your e-mail\n"
-                + "What is the filepath to the purchase history of this account?\n"
-                + "What is the filepath to the shopping cart for this account?\n"
-                + "Account made!\n"
-                + "Would you like you login or create a new account?\n"
-                + "1.Login\n"
-                + "2.Create new account\n"
-                + "Are you a Seller or Buyer\n"
-                + "Enter your new username\n"
-                + "Error: Username already exists. Pick a new username\n"
-                + "Enter your new username"
-                + "Enter your new password\n"
-                + "Enter your e-mail\n"
-                + "What is the filepath to the purchase history of this account?\n"
-                + "What is the filepath to the shopping cart for this account?"
-                + "Account made!\n"
-                +"Would you like you login or create a new account?\n"
-                + "1.Login\n"
-                + "2.Create new account\n"
-                + "Do you want to login as a Seller or Buyer\n"
-                + "Enter your username\n"
-                + "Enter your password\n"
-                + "Login successful!\n"
-                + "Enter the number of pieces player 2 used for building on day 7\n"
-                + "Enter the number of pieces player 1 used for building on day 8\n"
-                + "Enter the number of pieces player 2 used for building on day 8\n"
-                + "Enter the number of pieces player 1 used for building on day 9\n"
-                + "Enter the number of pieces player 2 used for building on day 9\n"
-                + "Enter the number of pieces player 1 used for building on day 10\n"
-                + "Enter the number of pieces player 2 used for building on day 10\n"
-                + "Congratulations to player 2 for winning the Lego Set Competition!\n"
-                + "Additional information about the competition results is below\n"
-                + "Player 1 completed the following sets: Tower Bridge, Eiffel Tower\n"
-                + "Player 1 did not complete the following sets: Ship in Bottle\n"
-                + "Player 1 built a total of 6000 pieces\n"
-                + "Player 2 completed the following sets: Tower Bridge, Eiffel Tower, Ship in Bottle\n"
-                + "Player 2 did not complete the following sets: None\n"
-                + "Player 2 built a total of 7000 pieces\n"
-                + "The competition lasted 10 days";
-
-        // Runs the program with the input values
-        receiveInput(input);
-        MarketPlace.main(new String[0]);
-
-        // Retrieves the output from the program
-        String output = getOutput();
-
-        // Trims the output and verifies it is correct.
-        output = output.replace("\r\n", "\n");
-        assertEquals("Ensure that your results match the format of the ones given in the handout!",
-                expected.trim(), output.trim());
-
-    }
+}
 }
